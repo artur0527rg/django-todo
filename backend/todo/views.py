3912df387bc99fdea76c1f7d87eb16e084c10f86
artur0527rg <rogozinskiyartur@gmail.com>
2023-08-28 16:48:06 +0300

    Sign up validation; Records per user limit

--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
+from rest_framework.exceptions import ValidationError
 
 from .models import Group, ToDo
 from .serializers import GroupSerializer, ToDoSerializer
@@ -8,13 +9,23 @@ from .serializers import GroupSerializer, ToDoSerializer
 class GroupViewSet(viewsets.ModelViewSet):
     '''
     An endpoint that allows you to create
-    a new group or get and edit your own groups
+    a new group or get and edit your own groups.
     '''
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
 
     def perform_create(self, serializer):
+        """
+        Checking how many groups a user has.
+        """
+        max_records = 2
         user = self.request.user
+        
+        groups_count = Group.objects.filter(owner=user).count()
+        if groups_count >= max_records:
+            raise ValidationError({
+                'detail': f'You cannot have more than {max_records} groups',
+            })
         serializer.save(owner=user)
     
     def get_queryset(self):
@@ -25,17 +36,29 @@ class GroupViewSet(viewsets.ModelViewSet):
 class ToDoViewSet(viewsets.ModelViewSet):
     '''
     Edpoint that allows the user to
-    interact with the todos in their groups
+    interact with the todos in their groups.
     '''
     queryset = ToDo.objects.all()
     serializer_class = ToDoSerializer
 
     def perform_create(self, serializer):
+        """
+        Checking the existence of a group
+        and the number of entries in it.
+        """
+        max_records = 1
+        user = self.request.user
+
         group = get_object_or_404(
             Group,
             pk=self.kwargs['group_id'],
             owner = self.request.user,
             )
+        groups_count = group.todo_set.count()
+        if groups_count >= max_records:
+            raise ValidationError({
+                'detail': f'You cannot have more than {max_records} todo records',
+            })
         serializer.save(group=group)
 
     def get_queryset(self):
--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+import django.contrib.auth.password_validation as validators
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth import get_user_model # If used custom user model
 from rest_framework.validators import UniqueValidator
@@ -6,7 +7,8 @@ from rest_framework.validators import UniqueValidator
 UserModel = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    email = serializers.EmailField()
+    password = serializers.CharField(style={'input_type': 'password'}, write_only=True)
 
     class Meta:
         model = UserModel
@@ -15,8 +17,6 @@ class UserSerializer(serializers.ModelSerializer):
     def validate_password(self, value: str) -> str:
         """
         Hash value passed by user.
-
-        :param value: password of a user
-        :return: a hashed version of the password
         """
+        validators.validate_password(password=value)
         return make_password(value)
