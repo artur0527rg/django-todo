279b6c8d97d2dd1358f7e480ac48f6063e2a7c79
artur0527rg <rogozinskiyartur@gmail.com>
2023-08-29 19:04:53 +0300

    User avatar

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,6 +1,18 @@
 from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
+from django_resized import ResizedImageField
+
 
 # Create your models here.
 class User(AbstractUser):
    email = models.EmailField(unique=True, null=False, blank=False)
+   avatar = ResizedImageField(
+      size=[720, 720],
+      crop=['middle', 'center'],
+      upload_to=settings.MEDIA_ROOT,
+      null=False,
+      blank=False,
+      default='avatar.png',
+      force_format='PNG',
+   )
--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -2,17 +2,15 @@ from rest_framework import serializers
 import django.contrib.auth.password_validation as validators
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth import get_user_model # If used custom user model
-from rest_framework.validators import UniqueValidator
 
 UserModel = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
-    email = serializers.EmailField()
     password = serializers.CharField(style={'input_type': 'password'}, write_only=True)
 
     class Meta:
         model = UserModel
-        fields = ( "id", "username", "password", "email",)
+        fields = ( "id", "username", "password", "email", 'avatar',)
 
     def validate_password(self, value: str) -> str:
         """
@@ -20,3 +18,7 @@ class UserSerializer(serializers.ModelSerializer):
         """
         validators.validate_password(password=value)
         return make_password(value)
+    
+    def validate_avatar(self, value):
+        value.name = 'avatar'
+        return value
