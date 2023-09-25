762a46fa8329746dd71d0cb8890ce630fcd84210
artur0527rg <rogozinskiyartur@gmail.com>
2023-09-25 16:13:21 +0300

    Readme; token_lifetime

--- backend/django_todo/settings.py
+++ backend/django_todo/settings.py
@@ -148,8 +148,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(hours=1),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(weeks=4),
+    "REFRESH_TOKEN_LIFETIME": timedelta(weeks=4),
 }
 
 MEDIA_URL = 'media/'
